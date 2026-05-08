cf7877f254c91c9d57a52f059fad115764fbf016
yys7517 <yys7517@naver.com>
2026-05-08 14:55:20 +0900

    initial commit

---
+++ src/main/java/org/example/bucketsearch/BucketsearchApplication.java
@@ -0,0 +1,13 @@
+package org.example.bucketsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BucketsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bucketsearch/BucketsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bucketsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
