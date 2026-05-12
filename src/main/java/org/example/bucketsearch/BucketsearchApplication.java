1154c40b67b8b541f32dfd7f65dbc5f124fefca3
yys7517 <yys7517@naver.com>
2026-05-12 09:43:35 +0900

    feature: BaseEntity(createdAt, updatedAt) 적용

--- src/main/java/org/example/bucketsearch/BucketsearchApplication.java
+++ src/main/java/org/example/bucketsearch/BucketsearchApplication.java
@@ -2,8 +2,10 @@ package org.example.bucketsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BucketsearchApplication {
 
     public static void main(String[] args) {
